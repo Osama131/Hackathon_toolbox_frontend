52ff0cea1e7306c4a3e2fb2b9c1750442187de12
Osama <osama.moharam@stud.uni-due.de>
2024-03-30 14:05:48 +0200

    setup compose web, db, storage, nginx & web

---
+++ db/init-mongo.js
@@ -0,0 +1,10 @@
+db.createUser({
+    user: "admin",
+    pwd: "SQR]BHB{6]>;ro:$Z@sH^h1[]^Bsu+",
+    roles: [
+        {
+            role: "readWrite",
+            db: "hackathon"
+        }
+    ]
+});
