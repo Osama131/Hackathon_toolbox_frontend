86c3ae8b9a63a7ac96bb0751c9086eb3afd9fdac
Osama <osama.moharam@stud.uni-due.de>
2024-04-24 11:37:46 +0200

    completed first draft of git intro

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
         <Link
           href="/git_intro"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
+          // target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
