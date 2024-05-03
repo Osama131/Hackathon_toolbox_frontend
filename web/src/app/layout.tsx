cc5b3614e811fe159b72b2ecc57083885aea6548
Osama <osama.moharam@stud.uni-due.de>
2024-05-03 13:33:18 +0200

    added colaps logo

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -17,9 +17,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/logo_colaps_bw.svg" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
