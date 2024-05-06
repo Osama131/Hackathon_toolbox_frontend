9ceee71f641d7b1e20c7990a9a3ce22bcf82fc68
Osama <osama.moharam@stud.uni-due.de>
2024-05-06 18:34:39 +0200

    chore: Update questionnaire link in home page

--- web/src/app/home/Links.tsx
+++ web/src/app/home/Links.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link"
 import LinkCard from "./components/LinkCard"
+import { cookies } from 'next/headers'
 
 const Links = () => {
+
+  const cookieStore = cookies()
+  const uuid = cookieStore.get('session')?.value;
+
   return (
     <section className="mb-32 grid gap-3 text-center lg:max-w-5xl lg:w-full lg:mb-0 grid-cols-2 lg:text-left">
       <LinkCard title={"Tutorials"} description={"Review current tutorials (Draft only!!)"} to={"/git_intro"}/>
       {/* FIXME: Replace dummy text and link with real questionnaire data */}
-      <LinkCard title={"Questionnaire"} description={"Lorem ipsum dolor sit amet"} to={"/"}/>
+      <LinkCard title={"Questionnaire"} description={"Help Us with our research by filling the following questionnaire"} to={"https://limesurvey.uni-due.de/index.php/618686?lang=en&uuid=" + uuid}/>
     </section>
   )
 }
