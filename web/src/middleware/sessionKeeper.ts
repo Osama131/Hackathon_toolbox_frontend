ecc1a5b408bcb48cd2fcccb644285dbd498b2373
Osama Moharam <osama.moharam@stud.uni-due.de>
2024-06-24 20:41:16 +0200

    connect uuid generation to database to store all aof them and avoid duplications

--- web/src/middleware/sessionKeeper.ts
+++ web/src/middleware/sessionKeeper.ts
@@ -1,8 +1,8 @@
 import { NextResponse, NextRequest } from 'next/server'
 import { cookies } from 'next/headers'
-import { v4 as uuidv4 } from 'uuid';
 
-export default function sessionKeeper(request: NextRequest) {
+
+export default async function sessionKeeper(request: NextRequest) {
   const response = NextResponse.next()
 
   // Check if the session cookie is already set
@@ -10,17 +10,34 @@ export default function sessionKeeper(request: NextRequest) {
   const hasCookie = cookieStore.has('session');
   if (!hasCookie) {
     // Generate a new session cookie
-    const session = generateNewUserToken()
+    const url = request.nextUrl.clone()
+    url.pathname = '/api/uuid'
+    const session = await generateNewUserToken(url)
     // Set the session cookie
     response.cookies.set('session', session, { expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000) });
+  } else {
+    // Workaround for devices that could access the site without before the latest modification about the session cookie
+    // POST to /api/uuid
+    const url = request.nextUrl.clone()
+    url.pathname = '/api/uuid'
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ uuid: cookieStore.get('session')?.value }),
+    })
   }
   return response
 }
 
-function generateNewUserToken() {
-  // Generate a new UUID
-  const uuid = uuidv4();
+async function generateNewUserToken(apiRoute: any): Promise<string> {
+  // fetch from /api/uuid
+  const response = await fetch(apiRoute, {
+    method: 'GET'
+  })
+  const data = await response.json()
+  const uuid = data.uuid
 
-  // Return the generated UUID
   return uuid;
 }
